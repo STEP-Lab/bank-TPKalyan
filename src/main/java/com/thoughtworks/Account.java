2289af7099025e32cf4e3405bffdd11849037496
pawan <pavankalyanthota@gmail.com>
2018-03-20 11:55:59 +0530

    throwing errors for invalid account name and lesser minimum balance

--- src/main/java/com/thoughtworks/Account.java
+++ src/main/java/com/thoughtworks/Account.java
@@ -4,7 +4,11 @@ public class Account {
     private final String accountNo;
     private int balance;
 
-    public Account(String accountNo, int balance) throws MinimumBalanceException {
+    public Account(String accountNo, int balance) throws MinimumBalanceException, InvalidAccountNumberException {
+        if(!this.isValidAccountNo(accountNo)){
+            System.out.println(this.isValidAccountNo(accountNo)+accountNo);
+            throw new InvalidAccountNumberException();
+        }
         if(balance<1000){
             throw new MinimumBalanceException();
         }
@@ -18,5 +22,9 @@ public class Account {
 
     public String getAccountNo() {
         return this.accountNo;
+    }
+
+    private Boolean isValidAccountNo(String accountNo){
+        return accountNo.matches("([0-9]){4}-([0-9]){4}");
     }
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,4 +1,5 @@
 import com.thoughtworks.Account;
+import com.thoughtworks.InvalidAccountNumberException;
 import com.thoughtworks.MinimumBalanceException;
 import org.junit.Test;
 
@@ -7,19 +8,24 @@ import static org.junit.Assert.assertEquals;
 public class AccountTest {
     @Test
 
-    public void getBalance() throws MinimumBalanceException {
-        Account account = new Account("1234", 1024);
+    public void getBalance() throws MinimumBalanceException, InvalidAccountNumberException {
+        Account account = new Account("1234-4321", 1024);
         assertEquals(1024,account.getBalance());
     }
 
     @Test
-    public void getAccountNo() throws MinimumBalanceException {
-        Account account = new Account("4321", 5000);
-        assertEquals("4321",account.getAccountNo());
+    public void getAccountNo() throws MinimumBalanceException, InvalidAccountNumberException {
+        Account account = new Account("4321-1234", 5000);
+        assertEquals("4321-1234",account.getAccountNo());
     }
 
     @Test(expected = MinimumBalanceException.class)
-    public void minimumBalanceException() throws MinimumBalanceException {
-        new Account("543",199);
+    public void minimumBalanceException() throws MinimumBalanceException, InvalidAccountNumberException {
+        Account account = new Account("5438-9870", 199);
+    }
+
+    @Test(expected = InvalidAccountNumberException.class)
+    public void invalidAccountNumberException() throws MinimumBalanceException, InvalidAccountNumberException {
+        new Account("12",9000);
     }
 }
